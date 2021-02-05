a0d308d609c07de9a74aa1ad1950399ee2965bf9
Ermal Bujupaj <ermal.bj@gmail.com>
2021-02-05 23:10:12 +0100

    First commit

---
+++ Taskly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Taskly
+//
+//  Created by Ermal Bujupaj on 5.2.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

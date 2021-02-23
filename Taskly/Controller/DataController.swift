48bb2842aeef394710ee90d468150f6de20cbcd0
Ermal Bujupaj <ermal.bj@gmail.com>
2021-02-23 21:59:39 +0100

    Remove old Task model file.

--- Taskly/Controller/DataController.swift
+++ Taskly/Controller/DataController.swift
@@ -10,26 +10,4 @@ import Foundation
 class DataController: ObservableObject {
     
     static var shared = DataController()
-    
-    @Published var tasks: [TTask] = [test]
-    @Published var completedTasks: [TTask] = []
-    
-    func saveTask(task: TTask) {
-        
-    }
-    
-    func completeTask(id: String) {
-       
-    }
-    
-    func deleteTask(id: String) {
-        
-    }
 }
-
-
-var test: TTask = {
-    var t = TTask()
-    t.title = "Ermal"
-    return t
-}()
